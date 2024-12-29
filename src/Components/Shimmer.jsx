b9e78bfc902695ba04ed6267030493bc3a982f7b
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-12-29 22:01:08 +0530

    update margin

--- src/Components/Shimmer.jsx
+++ src/Components/Shimmer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Shimmer = () => {
   return (
-    <div className='flex flex-wrap mx-10 text-center'>
+    <div className='flex flex-wrap ml-12 text-center'>
         <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
         <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
         <div className='w-32 h-48 bg-gray-200 border rounded-xl m-2'></div>
