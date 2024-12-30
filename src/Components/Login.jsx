a82baea251a1a583c62db4ac7990e1055d2dd296
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2024-12-30 13:22:20 +0530

    Add Toggle Login SignOut

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -9,10 +9,10 @@ const Login = () => {
     setSignUp(!signUp)
   }
 
-  const handleContinue = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault(); 
     navigate("/")
   }
-
   const onlineStatus = useOnlineStatus()
 
     return !onlineStatus ? <h1>Looks like you're offline! Please check your internet connection.</h1> : (
@@ -20,18 +20,21 @@ const Login = () => {
       <h3 className='text-center font-bold text-3xl'>{signUp ? "Sign Up" : "Sign In"}</h3>
 
       <div>
-        <form action="" className='flex flex-col my-6' onSubmit={(e) => e.preventDefault()}>
+        <form action="" className='flex flex-col my-6' onSubmit={handleSubmit}>
           {signUp && <label htmlFor="name" className='mx-4 font-semibold' >Full Name: </label>}
           {signUp && <input type="text" 
           id='name'
+         
           required
           placeholder='Enter your name '
           className='border border-black rounded-sm  mx-4 my-2 p-2 outline-none'  
            />}
           <label htmlFor="mobile" className='mx-4 font-semibold' > Mobile No :</label>
-          <input type="number" 
+          <input type="text"
+          inputMode='numeric' 
           id='mobile'
           required
+          maxLength={10}
           placeholder='Mobile Number'
           className='border border-black rounded-sm mx-4 my-2 p-2 outline-none' 
            />
@@ -49,7 +52,7 @@ const Login = () => {
           placeholder='Email'
           className='border border-black mx-4 rounded-sm p-2 outline-none' 
            />}
-           <button className='bg-red-700 rounded-lg p-2 mx-8 my-4 font-bold text-white active:bg-red-500'  onClick={handleContinue}>Continue</button>
+           <button className='bg-red-700 rounded-lg p-2 mx-8 my-4 font-bold text-white active:bg-red-500'  >Continue</button>
         </form>
         <h2 className='text-red-600 mx-2 my-2 font-semibold underline'>Forgot Password</h2>
         <h1 className='text-center font-semibold text-2xl '>OR</h1>
