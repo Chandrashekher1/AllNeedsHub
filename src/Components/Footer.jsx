88d94e30d9d5fa4f64a5e1b0e868e99aead8d320
Chandrashekher1 <146510072+Chandrashekher1@users.noreply.github.com>
2025-06-21 22:13:15 +0530

    remove map integration

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -30,14 +30,14 @@ const Footer = () => {
         <h2 className="font-semibold text-xl text-center mb-4">Social Links</h2>
         <div className="flex flex-col items-center">
           <SocialIcons />
-          <h3 className="font-semibold text-2xl underline my-4">Maps</h3>
+          {/* <h3 className="font-semibold text-2xl underline my-4">Maps</h3>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3507.2771358256023!2d77.1402501!3d28.471199400000003!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1f79ea6b8cef%3A0x685cdd68215ac6f9!2sChandu%20General%20Store!5e0!3m2!1sen!2sin!4v1735485541241!5m2!1sen!2sin"
             allowFullScreen
             loading="lazy"
             referrerPolicy="no-referrer-when-downgrade"
             className="w-full md:w-[600px] h-60 rounded-md"
-          ></iframe>
+          ></iframe> */}
         </div>
       </div>
 
